995982668f525022a36a9da8ecfd6d04546e4a6d
Andre-Grassi <andregrassi.j@gmail.com>
2025-06-19 21:19:25 -0300

    feat: adiciona classe Map e Coordinate

--- game.cpp
+++ game.cpp
@@ -1,58 +1,67 @@
-#include "network.hpp"
-#include <stdio.h>
-#include <string.h>
-#include <bitset>
+#include "game.hpp"
+#include <cstring>
 
-using std::bitset;
-
-struct teste
+Coordinate::Coordinate(uint8_t x, uint8_t y)
 {
-    uint8_t marcador_inicio;
-    uint8_t tamanho : 7;
-    uint8_t sequencia : 5;
-    uint8_t tipo : 4;
-    uint8_t checksum;
-    uint8_t *data;
-};
+    this->x = x;
+    this->y = y;
+}
 
-int main()
+Coordinate::Coordinate()
 {
+    this->x = 0;
+    this->y = 0;
+}
 
-    uint8_t buffer_data[1] = {0b11111111};
-    size_t tamanho_buffer = sizeof(buffer_data) / sizeof(buffer_data[0]);
-    // Coloca cada campo da struct teste dentro de um buffer
-    teste meu_pacote = {0b01111110, 0b1111111, 0b00000, 0b1111, 0b00000000, buffer_data};
+Map::Map(bool is_player)
+{
+    // Inicializa grid com EMPTY
+    memset(grid, EMPTY, sizeof(grid));
 
-    uint32_t pacote_em_32bits = 0;
+    player_position = Coordinate(0, 0);
+    grid[0][0] = PLAYER;
 
-    pacote_em_32bits |= (uint32_t)meu_pacote.marcador_inicio;
-    pacote_em_32bits |= (uint32_t)meu_pacote.tamanho << 8;
-    pacote_em_32bits |= (uint32_t)meu_pacote.sequencia << 15;
-    pacote_em_32bits |= (uint32_t)meu_pacote.tipo << 20;
-    pacote_em_32bits |= (uint32_t)meu_pacote.checksum << 24;
-
-    uint8_t *pacote_final = new uint8_t[4 + tamanho_buffer];
-    // Copia os 4 bytes do pacote em 32 bits para o pacote final
-    for (int i = 0; i < 4; ++i)
+    if (!is_player)
     {
-        pacote_final[i] = (pacote_em_32bits >> (i * 8)) & 0xFF;
-    }
-    // Copia os dados do buffer para o pacote final
-    for (size_t i = 0; i < tamanho_buffer; ++i)
-    {
-        pacote_final[4 + i] = buffer_data[i];
-    }
-
-    // Imprime os bits do pacote final
-    for (size_t i = 0; i < 4 + tamanho_buffer; ++i)
-    {
-        printf("Byte %zu: ", i);
-        for (int j = 7; j >= 0; --j)
+        // Inicializa tesouros em posições aleatórias
+        for (int i = 0; i < NUM_TREASURES; ++i)
         {
-            printf("%d", (pacote_final[i] >> j) & 1);
+            int x, y;
+            do
+            {
+                x = rand() % GRID_SIDE;
+                y = rand() % GRID_SIDE;
+            } while (grid[x][y] != EMPTY); // Garante que a posição esteja vazia
+
+            treasures[i] = Coordinate(x, y);
+            grid[x][y] = TREASURE;
+        }
+    }
+}
+
+void Map::print()
+{
+    for (int y = GRID_SIDE - 1; y >= 0; --y)
+    {
+        for (int x = 0; x < GRID_SIDE; ++x)
+        {
+            switch (grid[x][y])
+            {
+            case EMPTY:
+                printf(" . ");
+                break;
+            case PLAYER:
+                printf(" P ");
+                break;
+            case VISITED:
+                printf(" V ");
+                break;
+            case TREASURE:
+                printf(" T ");
+                break;
+            }
+            printf("|");
         }
         printf("\n");
     }
-
-    return 0;
 }
---
+++ game.hpp
@@ -0,0 +1,40 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "network.hpp"
+
+#define GRID_SIDE 8
+#define NUM_TREASURES 8
+#define TREASURE_DIR "objetos/"
+
+class Coordinate
+{
+public:
+    uint8_t x;
+    uint8_t y;
+
+    Coordinate();
+    Coordinate(uint8_t x, uint8_t y);
+};
+
+enum cell_type
+{
+    EMPTY,
+    PLAYER,
+    VISITED,
+    TREASURE,
+};
+
+class Map
+{
+public:
+    cell_type grid[GRID_SIDE][GRID_SIDE];
+    Coordinate treasures[NUM_TREASURES];
+    Coordinate player_position;
+
+    Map(bool is_player);
+
+    void print();
+};
+
+#endif
