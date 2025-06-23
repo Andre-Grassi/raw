4dd2fe9eaf0a1aa4a98a88fd3be3e56e162a5eb2
Andre Grassi <andregrassi.j@gmail.com>
2025-06-23 23:38:24 +0000

    fix: corrige segfault ao se movimentar para lado inválido

--- player.cpp
+++ player.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
     srand(0);
 
-    Network net = Network("enp0s31f6");
+    Network net = Network("enp2s0");
 
     Map map = Map(true); // Player mode
 
@@ -30,47 +30,49 @@ int main(int argc, char *argv[])
 
             // Lê do teclado apenas WASD
             char input;
-            std::cin >> input;
-
-            message_type move;
-
             bool is_valid_move = false;
-
-            if (input == 'w' || input == 'a' || input == 's' || input == 'd')
+            message_type move;
+            do
             {
-                switch (input)
+                std::cin >> input;
+
+
+                is_valid_move = false;
+
+                if (input == 'w' || input == 'a' || input == 's' || input == 'd')
                 {
-                case 'w': // Cima
-                    move = UP;
-                    break;
-                case 's': // Baixo
-                    move = DOWN;
-                    break;
-                case 'a': // Esquerda
-                    move = LEFT;
-                    break;
-                case 'd': // Direita
-                    move = RIGHT;
-                    break;
+                    switch (input)
+                    {
+                    case 'w': // Cima
+                        move = UP;
+                        break;
+                    case 's': // Baixo
+                        move = DOWN;
+                        break;
+                    case 'a': // Esquerda
+                        move = LEFT;
+                        break;
+                    case 'd': // Direita
+                        move = RIGHT;
+                        break;
+                    }
+
+                    // Process the move
+                    is_valid_move = map.move_player(move);
+                }
+                else
+                {
+                    printf("Invalid input. Use only 'w', 'a', 's', or 'd'.\n");
+                    continue;
                 }
 
-                // Process the move
-                is_valid_move = map.move_player(move);
-            }
-            else
-            {
-                printf("Invalid input. Use only 'w', 'a', 's', or 'd'.\n");
-                continue;
-            }
-            if (is_valid_move)
-            {
-                map.print();
-                Message message = Message(0, net.my_sequence, move, NULL);
-                returned_message = net.send_message(&message);
-            }
-            else
-                puts("Can't move in that direction. Try another one.");
-
+                if (!is_valid_move)
+                    puts("Can't move in that direction. Try another one.");
+            } while (!is_valid_move);
+            
+            map.print();
+            Message message = Message(0, net.my_sequence, move, NULL);
+            returned_message = net.send_message(&message);
             switch (returned_message->type)
             {
             case ACK:
